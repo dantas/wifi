14c8162e6e85b3c2b42970d60cd58a5d10504aff
Eduardo Damian Dantas <dantas@e3c.com.br>
2014-02-26 18:37:22 -0300

    Import modules from the current directory

--- wifi/utils.py
+++ wifi/utils.py
@@ -3,7 +3,7 @@ from __future__ import print_function, unicode_literals, division
 import os
 import re
 import sys
-import wifi.subprocess_compat as subprocess
+import subprocess_compat as subprocess
 
 
 if sys.version < '3':
